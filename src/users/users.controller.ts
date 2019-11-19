c17fca08bb6f1628ac9ae76509cb1a8bcb8253cf
Sergey Shapirenko <S.Shapirenko@ridetransfer.com>
2019-11-19 19:11:28 +0700

    add read method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
-
-    @Get()
-    getUsers(): string {
-        return this.usersService.getUsers();
-    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,12 @@ import { User } from './user.interface';
 export class UsersService {
     constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
-    getUsers(): string {
-        return 'Hello users!';
+    async createUser(name: string, age: number, role: string): Promise<User> {
+        const user = new this.userModel({ name, age, role });
+        return await user.save();
+    }
+
+    async getAllUsers() {
+        return await this.userModel.find().exec();
     }
 }
