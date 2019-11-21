0e53dac85fe32ce9fab8effb9f4e373071c13ded
Sergey Shapirenko <S.Shapirenko@ridetransfer.com>
2019-11-21 15:18:33 +0700

    add rest crud methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Patch,
+    Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+
+import { User } from './user.interface';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    async createUser(
+        @Body('name') name: string,
+        @Body('age') age: number,
+        @Body('price') role: string
+    ): Promise<User> {
+        return await this.usersService.createUser(name, age, role);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        return await this.usersService.getAllUsers();
+    }
+
+    @Get(':id')
+    async getUser(@Param('id') id: string): Promise<User> {
+        return await this.usersService.getUser(id);
+    }
+
+    @Patch(':id')
+    async updateUser(
+        @Param('id') id: string,
+        @Body('name') name: string,
+        @Body('age') age: number,
+        @Body('role') role: string
+    ): Promise<User> {
+        return await this.usersService.updateUser(id, name, age, role);
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<User> {
+        return await this.usersService.deleteUser(id);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { User } from './user.interface';
@@ -13,7 +13,39 @@ export class UsersService {
         return await user.save();
     }
 
-    async getAllUsers() {
+    async getAllUsers(): Promise<User[]> {
         return await this.userModel.find().exec();
+    }
+
+    async getUser(id: string): Promise<User> {
+        const user = await this.userModel.findById(id).exec();
+
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+    async updateUser(
+        id: string,
+        name?: string,
+        age?: number,
+        role?: string
+    ): Promise<User> {
+        const user = await this.userModel.updateOne(id, { name, age, role }).exec();
+
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+    async deleteUser(id: string): Promise<User> {
+        const user = await this.userModel.findByIdAndDelete(id).exec();
+
+        if (!user) {
+            throw new NotFoundException('user nor found');
+        }
+        return user;
     }
 }
