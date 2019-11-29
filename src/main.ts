76714d7bed1e915ecd82732d0995d734416f45c2
Sergey Shapirenko <S.Shapirenko@ridetransfer.com>
2019-11-29 17:35:27 +0700

    add proxy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import proxy from 'express-http-proxy';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
+    app.use('/', proxy('www.google.com'));
     await app.listen(3000);
 }
 bootstrap();
